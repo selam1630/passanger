5ab8c479cbbdc497f63ba54d232c7c204241f204
selam1630 <fikruselam29@gmail.com>
2025-10-10 16:18:42 +0300

    initialized folder structure

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Welcome to CarryConnect Backend!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
